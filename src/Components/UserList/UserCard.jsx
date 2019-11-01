8765407c6f7e426cd965adbc5569ee2d0fefa4c0
Luke Horne <luke.horne@live.co.uk>
2019-11-01 10:16:07 +0000

    Added default avatar for failed image urls

--- src/Components/UserList/UserCard.jsx
+++ src/Components/UserList/UserCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import defaultAvatar from '../../imgs/defaultAvatar.jpg';
 
 function UserCard({ user }) {
   const { username, avatar_url, name } = user;
+  const addDefaultAvatar = event => {
+    event.target.src = defaultAvatar;
+  };
   return (
     <section className="userCard" key={username}>
-      <img src={avatar_url} alt={`Avatar for ${username}`} />
+      <img src={avatar_url} alt={`Avatar for ${username}`} onError={addDefaultAvatar} />
       <p>{username}</p>
       <p>{name}</p>
     </section>
