d3e56274ea63a8845d137ad4d989fa2b81478a8d
Luke Horne <luke.horne@live.co.uk>
2019-11-04 09:56:59 +0000

    Added article and comment counts to user card

--- src/Components/UserList/UserCard.jsx
+++ src/Components/UserList/UserCard.jsx
@@ -3,16 +3,28 @@ import defaultAvatar from '../../imgs/defaultAvatar.jpg';
 import { Link } from '@reach/router';
 
 function UserCard({ user }) {
-  const { username, avatar_url, name } = user;
+  const { username, avatar_url, name, comment_count, article_count } = user;
   const addDefaultAvatar = event => {
     event.target.src = defaultAvatar;
   };
   return (
-    <Link to={`/u/${username}`} className="userCard" key={username}>
-      <img className="userAvatar" src={avatar_url} alt={`Avatar for ${username}`} onError={addDefaultAvatar} />
-      <p className="userUsername">{username}</p>
-      <p className="userName">{name}</p>
-    </Link>
+    <section className="userCard card" key={username}>
+      <Link className="userAvatar" to={`/u/${username}`}>
+        <img src={avatar_url} alt={`Avatar for ${username}`} onError={addDefaultAvatar} />
+      </Link>
+      <Link className="userUsername" to={`/u/${username}`}>
+        <p>{username}</p>
+      </Link>
+      <Link className="userName" to={`/u/${username}`}>
+        <p>{name}</p>
+      </Link>
+      <Link className="userArticleCount" to={`/u/${username}/articles`}>
+        <p>{article_count} articles</p>
+      </Link>
+      <Link className="userCommentCount" to={`/u/${username}/comments`}>
+        <p>{comment_count} comments</p>
+      </Link>
+    </section>
   );
 }
 
